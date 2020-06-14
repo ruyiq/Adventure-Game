a34c79e484e5fd2913358ba51c9debd1c8c80312
kaka <63922370+ruyiq@users.noreply.github.com>
2020-06-14 13:39:25 -0400

    add restart feature

--- THE GAME.py
+++ THE GAME.py
@@ -1,5 +1,7 @@
 import time
 import random
+import os
+import sys
 
 
 def print_pause(message: str, pause=2) -> any:
@@ -21,7 +23,7 @@ def valid_input(prompt, options=None):
     if choice in options:
         return choice
     else:
-        print_pause("Your previous answer is not valid. Please enter 1 or 2 "
+        print_pause("Your previous answer is not valid. Please enter 1 or 2"
                     "next time.")
         return valid_input(prompt, options)
 
@@ -42,6 +44,7 @@ def transportation(message: str) -> any:
     print_pause(f"enter 1 to {walk_option}")
     print_pause(f"enter 2 to {bus_option}")
     matching(valid_input('Please enter 1 or 2'), robbed_response, message)
+    restart_or_not()
 
 
 def check_result(guess, random_num) -> any:
@@ -61,28 +64,42 @@ def check_result(guess, random_num) -> any:
             return 'lose'
 
 
+def restart_or_not():
+    option = valid_input("DO you want to restart the game? Enter"
+                         "1 for yes and 2 for no", options=None)
+    if option is '1':
+        restart()
+    else:
+        sys.exit(0)
+
+
+def restart():
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
+
 #  Here are some important variables
-robbed_response = "You are so unlucky today! You were almost home but went " \
-                  "across the Queens Park and got robbed. Poor you. Let me" \
-                  " call 911 for you. [GAME OVER] "
+robbed_response = "You are so unlucky today! You were almost home " \
+                  "but went across the Queens Park and got robbed. " \
+                  "Poor you. Let me call 911 for you. [GAME OVER] "
 
-pity_message = "Congratulations! You arrived home safely! Its a pity that" \
-               " you didnt get to visit the park but its OK! GOOD NIGHT! " \
-               "[GAME OVER]"
+pity_message = "Congratulations! You arrived home safely! Its a " \
+               "pity that you didnt get to visit the park but " \
+               "its OK! GOOD NIGHT! [GAME OVER]"
 
-congratulation_message = "Congratulations! You are correct! Your trip home will"
-"be covered by the shop! Good night!"
+congratulation_message = "Congratulations! You are correct! Your " \
+                         "trip home will be covered by the shop! " \
+                         "Good night!"
 walk_option = 'go back home walking'
 bus_option = 'go back home by bus'
 safe_message = 'Congratulations! You arrived home safely! [GAME OVER]'
-sorry_message = "Sorry, you guessed wrong. Its already late at night you " \
-                "should go home now"
+sorry_message = "Sorry, you guessed wrong. Its already late " \
+                "at night you should go home now"
 
 
 # This is the introduction part. No action need to be taken from the client.
 print_pause(
-    "Rumor has it that a wicked fairie is somewhere around here, and has been "
-    "terrifying the nearby village.")
+    "Rumor has it that a wicked fairie is somewhere around here, and "
+    "has been terrifying the nearby village.")
 print_pause("In front of you is a theme park.")
 print_pause("To your right is a second-hand shop.")
 print_pause(
@@ -94,16 +111,19 @@ ans = valid_input("What would you like to do?  (Please enter 1 or 2). :")
 
 # park
 if ans == '1':
-    print_pause("The clerk says that the ticket is $50 but if you would like "
-                "to post sth related to the park on instagram then you can "
+    print_pause("The clerk says that the ticket is $50 but if "
+                "you would like to post sth related to the park"
+                "on instagram then you can "
                 "get it for $10. ")
     answer = valid_input("Enter 1 to do the ad. Enter 2 otherwise.")
-    response1 = "Oops! You didnt bring enough cash with you! I'm afraid you " \
-                "have to go back home now. You have 2 transportation options. "
-    response2 = "Congratulations! You only brought 12 dollars to the park and" \
-                "normally you would not be able to get in the park! " \
-                "Now, there are 2 options in front of you. You can " \
-                "either choose to go take roller coasters or go to the shops "
+    response1 = "Oops! You didnt bring enough cashn with you! I'm " \
+                "afraid you have to go back home now. You have" \
+                " 2 transportation options. "
+    response2 = "Congratulations! You only brought 12 dollars to " \
+                "the park and normally you would not be able to " \
+                "get in the park! Now, there are 2 options in " \
+                "front of you. You can either choose to go " \
+                "take roller coasters or go to the shops "
     matching(answer, response1, response2)
     # transportation
     if answer == '1':
@@ -111,34 +131,37 @@ if ans == '1':
     else:
         answer = valid_input(
             "What would you like to do?  (Please enter 1 or 2). :")
-        message1 = "You spend 2 hours waiting for your turn. After the ride, " \
-                   "its already 8pm and you are asked to leave. "
-        message2 = "There is a activity going on. The clerk will randomly " \
-                   "come up with a number. He/she will write it down without " \
-                   "showing it to you and your job is to guess whether it is " \
-                   "odd or even "
+        message1 = "You spend 2 hours waiting for your turn. After " \
+                   "the ride, its already 8pm and you are asked to leave. "
+        message2 = "There is a activity going on. The clerk will " \
+                   "randomly come up with a number. He/she will write" \
+                   "it down without showing it to you and your job is" \
+                   "to guess whether it is odd or even "
         matching(answer, message1, message2)
         if answer == '1':
             transportation(safe_message)
         else:
-            your_ans = valid_input("Enter 1 if you think it is odd. Enter 2 "
-                                   "otherwise.")
+            your_ans = valid_input("Enter 1 if you think it is odd. "
+                                   "Enter 2 otherwise.")
             random_num = random.randint(1, 10000)
             result = check_result(your_ans, random_num)
             if result is 'win':
                 print_pause(congratulation_message)
+                restart_or_not()
             else:
                 print_pause(sorry_message)
                 transportation(safe_message)
 # shop
 if ans == '2':
     print_pause("They don't really want one and they offered a price of $2. "
-                "DO YOU WANT TO SELL IT OR YOU WANT TO BRING IT BACK TO HOME?")
+                "DO YOU WANT TO SELL IT OR YOU WANT TO BRING IT BACK"
+                "TO HOME?")
     ans = valid_input('enter 1 to sell. 2 otherwise.')
 
     if ans == '1':
-        print_pause("Congratulations! You've earned 2 dollars today. However, "
-                    "is too late and you are refused from going into the park. "
+        print_pause("Congratulations! You've earned 2 dollars today. "
+                    "However, is too late and you are refused "
+                    "from going into the park."
                     "You have to go home now. ")
         transportation(pity_message)
     else:
